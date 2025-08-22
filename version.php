b2c04d28307b18147640a40a5b93d7952f236663
jchoquette <jeremie8@msn.com>
2025-08-22 11:06:19 -0400

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_courseflowtool';
-$plugin->version = 2025051601; // YYYYMMDDXX format.
+$plugin->version = 2025082201; // YYYYMMDDXX format.
 $plugin->requires = 2022112800; // Requires Moodle 4.1 or later.
 $plugin->maturity = MATURITY_ALPHA; // Change to MATURITY_STABLE when finalized.
-$plugin->release = '1.1.1';
+$plugin->release = '1.1.2';
